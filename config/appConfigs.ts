4fd5cdefc3f94f0e52d169abdc704ff96123261f
Yuriy Zhuk <zhukyuri@gmail.com>
2022-04-16 13:29:33 +0300

    [graphql] NestJS init

---
+++ config/appConfigs.ts
@@ -0,0 +1,40 @@
+// TOKENS
+
+export function dayToMsec(days: number): number {
+  return days * 24 * 60 * 60 * 1000;
+}
+
+export function minuteToMsec(minutes: number): number {
+  return minutes * 60 * 1000;
+}
+
+export function msecToMinuteStr(mSec: number): string {
+  return `${msecToMinute(mSec)}m`;
+}
+
+export function msecToSecondStr(mSec: number): string {
+  return `${msecToSecond(mSec)}s`;
+}
+
+export function msecToMinute(mSec: number): number {
+  return mSec / 1000 / 60;
+}
+
+export function msecToSecond(mSec: number): number {
+  return mSec / 1000;
+}
+
+export const authTwoFactoring = false;
+
+export const nameAccessToken = 'token';
+export const nameRefreshToken = 'refreshToken';
+
+export const expiresAccessToken = minuteToMsec(60);
+export const expiresAccessTokenSrt = msecToSecondStr(expiresAccessToken);
+
+export const expiresRefreshToken = minuteToMsec(24 * 60);
+export const expiresRefreshTokenStr = msecToSecondStr(expiresRefreshToken);
+
+// CORS
+
+export const corsList = ['http://localhost:3007', 'http://localhost:3000'];
